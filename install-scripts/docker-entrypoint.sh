4cb6d7cecc6dfc62f91ba241b09fa95829f1b1da
murn0 <47878863+murn0@users.noreply.github.com>
2023-08-18 17:37:40 +0900

    update.

--- install-scripts/docker-entrypoint.sh
+++ install-scripts/docker-entrypoint.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 set -e
 echo "Starting docker-entrypoint.sh"
+
+# OSの判定
+if [[ "$(cat /etc/os-release)" == *"Ubuntu"* ]]; then
+    # Ubuntuの場合
+    apt update
+    apt install -y make
+elif [[ "$(cat /etc/os-release)" == *"Alpine"* ]]; then
+    # Alpineの場合
+    apk update
+    apk add make
+else
+    echo "Unsupported OS."
+    exit 1
+fi
